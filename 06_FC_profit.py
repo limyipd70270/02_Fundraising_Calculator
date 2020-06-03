ea45bf7d15909dd8e6371a2db6e17a49155ac7f4
limyipd70270 <60630395+limyipd70270@users.noreply.github.com>
2020-06-04 09:55:18 +1200

    started profit

--- 06_FC_profit.py
+++ 06_FC_profit.py
@@ -1,11 +1,46 @@
+# Functions
+def not_blank(question, error_msg, num_ok):
+    error = error_msg
 
-# ***** Functions ******
+    valid = False
+    while not valid:
+        response = input(question)
+        has_errors = ""
+
+        if num_ok != "yes":
+            # look at each character in string and if it's a number, complain
+            for letter in response:
+                if letter.isdigit() == True:
+                    has_errors = "yes"
+                    break
+
+        if response == "":
+            print(error)
+            continue
+        elif has_errors != "":
+            print(error)
+        else:
+            return response
 
 
-# Ask how much profit they want (in $ or %)
-#
-#
-#
-#
-#
+# Number checking function
+def num_check(question, error_msg, type):
+    error = error_msg
 
+    valid = False
+    while not valid:
+        try:
+            response = type(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# Ask if their profit will be in $ or %)
+profit = ""
+profit_type = not_blank("Is your profit in '$' or '%'?", "Please enter '$' or '%'", "no")
